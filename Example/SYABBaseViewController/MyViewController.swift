00216c183d68ddf3369930f823fb1a8dac66da2e
xygj <wangyalong24@126.com>
2019-04-01 10:58:27 +0800

    update

---
+++ Example/SYABBaseViewController/MyViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MyViewController.swift
+//  SYABBaseViewController_Example
+//
+//  Created by xygj on 2019/3/26.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import SYABBaseViewController
+
+class MyViewController: SYABBaseWebViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
